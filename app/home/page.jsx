e7748b954d6fcfd7950282a1e41f592bcab7a09c
JcobV01 <jakubwadycki@gmail.com>
2024-09-18 14:15:24 +0200

    poczatek kontaktu

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,6 +1,7 @@
 import Footer from '@components/Footer'
 import Header from '@components/Header'
 import AboutUs from '@components/home/sections/AboutUs/AboutUs'
+import Contact from '@components/home/sections/Contact/Contact'
 import Cooperation from '@components/home/sections/Cooperation/Cooperation'
 import Encourage from '@components/home/sections/Encourage/Encourage'
 import Portofolio from '@components/home/sections/Portfolio/Portofolio'
@@ -15,7 +16,8 @@ const home = () => {
         <AboutUs />
         <Cooperation />
         <Portofolio/>
-
+        <Contact/>
+        
         <Footer />
 
         <div className='absolute top-0 left-0 w-full h-full background'></div>
