3471332ff19284083a2c453c2b01ce93f089bea0
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-09-24 12:59:25 +0200

    Media do ostatniej sekcji w portfolio - najtrudniejszej

--- components/portfolio/RealizationRowTwo.jsx
+++ components/portfolio/RealizationRowTwo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Realization = ({title, desc, arrow, accent}) => {
+const RealizationRowTwo = ({title, desc, arrow, accent}) => {
   return (
     <div className='w-[270px] h-[150px] xl:w-[200px] lg:basis-1/3'>
         <div className='flex gap-[28px] items-center relative mb-[15px] lg:justify-center'>
             <div className='w-[30px] h-[30px] rounded-full bg-[#E2B350] shadow-[0px_0px_10px_#E2B35080]'></div>
             <div className={`h-[2px] bg-[#959595] relative ${arrow == 'line' ? 'w-[50%]' : 'flex-1'} lg:hidden`}>
-                <span className={`absolute ${arrow == 'line' ? 'hidden' : arrow == 'left' ? 'left-[-10px] rotate-180' : 'right-[-10px]'} top-[50%] translate-y-[-50%] w-0 h-0 border-t-[5px] border-t-transparent border-b-[5px] border-b-transparent border-l-[10px] border-l-[#959595] inline-block`}></span>
+                <span className={`absolute ${arrow == 'left' ? 'right-[-10px] rotate-180' : 'left-[-10px] rotate-180'} top-[50%] translate-y-[-50%] w-0 h-0 border-t-[5px] border-t-transparent border-b-[5px] border-b-transparent border-l-[10px] border-l-[#959595] inline-block`}></span>
                 <p className='absolute left-[15px] top-[-25px] text-[#B8B8B8] text-[12px]'>{accent}</p>
             </div>
         </div>
@@ -19,4 +19,4 @@ const Realization = ({title, desc, arrow, accent}) => {
   )
 }
 
-export default Realization
+export default RealizationRowTwo
--- components/portfolio/sections/PortfolioSteps.jsx
+++ components/portfolio/sections/PortfolioSteps.jsx
@@ -1,29 +1,24 @@
 import Realization from '../Realization'
+import RealizationRowTwo from '../RealizationRowTwo'
 
 const PortfolioSteps = ({ steps }) => {
-    const modifiedSteps = [...steps];
-
-    // Sprawdzenie, czy indeksy 4 i 6 istnieją, i zamiana miejscami
-    if (modifiedSteps.length > 6) {
-        const temp = modifiedSteps[4];
-        modifiedSteps[4] = modifiedSteps[6];
-        modifiedSteps[6] = temp;
-    }
 
     return (
         <article className="w-[1240px] xl:w-[90%]">
-            <h5 className='text-[20px] tracking-[3px] text-[#E2B350] mb-[60px]'>JAK PRZEBIEGŁA REALIZACJA?</h5>
-            <div className='flex flex-wrap gap-[30px] gap-y-[60px] justify-end w-[1170px] m-auto relative xl:w-[90%]'>
-                {modifiedSteps.map((step, index) => {
-                    console.log(index)
-                    console.log(step)
-                    console.log(step.title)
+            <h5 className='text-[20px] tracking-[3px] text-[#E2B350] mb-[60px] sm:text-center'>JAK PRZEBIEGŁA REALIZACJA?</h5>
+            <div className='flex justify-start gap-[30px] w-[1170px] m-auto relative xl:w-[960px] lg:w-[90%] lg:justify-center lg:items-center lg:flex-wrap lg:gap-[50px] sm:flex-col'>
+                {steps.slice(0, 4).map((step, index) => (
+                    <Realization arrow={index === 3 ? 'line' : index === steps.length - 1 ? 'line' : index > 3 ? 'left' : 'right'} title={step.title} desc={step.description} accent={index === 0 ? 'przygotowanie' : index === 1 ? 'start' : index === steps.length - 1 ? 'koniec' : ''} key={index} />
+                ))}
+                <div className='absolute right-0 top-[14px] w-[100px] h-[212px] rounded-tr-[30px] rounded-br-[30px] border-r-[2px] border-r-[#959595] border-t-[2px] border-t-[#959595] border-b-[2px] border-b-[#959595] xl:right-[80px] lg:hidden'></div>
+            </div>
+            <div className='flex flex-row-reverse gap-[30px] mt-[60px] w-[1170px] m-auto relative xl:w-[960px] xl:pr-[70px] lg:w-[90%] lg:justify-center lg:items-center lg:flex-wrap lg:gap-[50px] lg:pr-[0px] sm:flex-col'>
+                {steps.slice(4).map((step, index) => {
+                    const isLast = index === steps.slice(4).length - 1
                     return (
-
-                        <Realization arrow={index === 3 ? 'line' : index === steps.length - 1 ? 'line' : index > 3 ? 'left' : 'right'} title={step.title} desc={step.description} accent={index === 0 ? 'przygotowanie' : index === 1 ? 'start' : index === steps.length - 1 ? 'koniec' : ''} key={index} />
+                        <RealizationRowTwo arrow={index === 0 ? 'line' : index === steps.length - 1 ? 'line' : index > 3 ? 'left' : 'right'} title={step.title} desc={step.description} accent={isLast ? 'koniec' : ''} key={index} />
                     )
                 })}
-                <div className='absolute right-0 top-[14px] w-[100px] h-[212px] rounded-tr-[30px] rounded-br-[30px] border-r-[2px] border-r-[#959595] border-t-[2px] border-t-[#959595] border-b-[2px] border-b-[#959595]'></div>
             </div>
         </article>
     )
