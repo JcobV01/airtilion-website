270a93856757a0cd90bb46da9ba35ca567174170
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-09-25 10:53:03 +0200

    Media do strony głównej

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ import Image from 'next/image'
 
 const page = () => {
 
-  
+
   useEffect(() => {
     const text = document.querySelector('.bouncingText');
     const letters = text.textContent.split('');
@@ -17,7 +17,7 @@ const page = () => {
 
     letters.forEach((letter, index) => {
       const span = document.createElement('span');
-      span.textContent = letter === ' ' ? '\u00A0' : letter; 
+      span.textContent = letter === ' ' ? '\u00A0' : letter;
       span.style.animationDelay = `${index * 0.1}s`;
       text.appendChild(span);
     });
@@ -26,44 +26,44 @@ const page = () => {
     const spans = text.querySelectorAll('span');
 
     function animateLetter() {
-      spans.forEach((span, index) => {
+      spans.forEach((span) => {
         span.classList.remove('active');
       });
       spans[currentIndex].classList.add('active');
 
-      currentIndex = (currentIndex + 1) % spans.length; 
+      currentIndex = (currentIndex + 1) % spans.length;
 
-      setTimeout(animateLetter, 1000); 
+      setTimeout(animateLetter, 1000);
     }
 
-    animateLetter(); 
+    animateLetter();
   }, []);
 
   return (
-    <section style={{backgroundImage: `url(${bgImage.src})`}} className='w-full h-full bg-cover'>
-        <div className='w-full h-full bg-[#0b0b0bef] flex items-center flex-col justify-between py-[50px]'>
-          
-          <div className='w-[600px] h-[600px] rounded-full border-[#B4A179] border-r-[2px] border-l-[2px] flex justify-center items-center'>
-            <Image src={logo} width={400} height="auto"/>
-          </div>
-          
-          <Link href="/home" className='relative h-[60px] px-[60px] text-[18px] flex justify-center items-center hover:scale-105 hover:text-[15px] duration-500'>
-            <span className='absolute h-[15px] top-0 left-0 w-[2px] bg-[#E2B350]'></span>
-            <span className='absolute h-[2px] top-0 left-0 w-[15px] bg-[#E2B350]'></span>
+    <section style={{ backgroundImage: `url(${bgImage.src})` }} className='w-full h-full bg-cover 2xl:bg-center'>
+      <div className='w-full h-full bg-[#0b0b0bef] flex items-center flex-col justify-between py-[50px]'>
 
-            <span className='absolute h-[15px] bottom-0 left-0 w-[2px] bg-[#E2B350]'></span>
-            <span className='absolute h-[2px] bottom-0 left-0 w-[15px] bg-[#E2B350]'></span>
-
-            <span className='absolute h-[15px] top-0 right-0 w-[2px] bg-[#E2B350]'></span>
-            <span className='absolute h-[2px] top-0 right-0 w-[15px] bg-[#E2B350]'></span>
-
-            <span className='absolute h-[15px] bottom-0 right-0 w-[2px] bg-[#E2B350]'></span>
-            <span className='absolute h-[2px] bottom-0 right-0 w-[15px] bg-[#E2B350]'></span>
-            PRZEJDŹ DALEJ
-          </Link>
-
-          <p className='font-[Jura] text-[20px] font-extralight text-[#E2B350] tracking-[22.5px] bouncingText'>PASSION MAKES IT BETTER</p>
+        <div className='w-[600px] h-[600px] rounded-full border-[#B4A179] border-r-[2px] border-l-[2px] flex justify-center items-center sm:w-[90%] sm:px-[20px] fold:border-none'>
+          <Image src={logo} width={400} height="auto" alt="Logo firmy" className='sm:w-[300px]'/>
         </div>
+
+        <Link href="/home" className='relative h-[60px] px-[60px] text-[18px] flex justify-center items-center hover:scale-105 hover:text-[15px] duration-500 sm:h-[50px] sm:text-[14px]'>
+          <span className='absolute h-[15px] top-0 left-0 w-[2px] bg-[#E2B350]'></span>
+          <span className='absolute h-[2px] top-0 left-0 w-[15px] bg-[#E2B350]'></span>
+
+          <span className='absolute h-[15px] bottom-0 left-0 w-[2px] bg-[#E2B350]'></span>
+          <span className='absolute h-[2px] bottom-0 left-0 w-[15px] bg-[#E2B350]'></span>
+
+          <span className='absolute h-[15px] top-0 right-0 w-[2px] bg-[#E2B350]'></span>
+          <span className='absolute h-[2px] top-0 right-0 w-[15px] bg-[#E2B350]'></span>
+
+          <span className='absolute h-[15px] bottom-0 right-0 w-[2px] bg-[#E2B350]'></span>
+          <span className='absolute h-[2px] bottom-0 right-0 w-[15px] bg-[#E2B350]'></span>
+          PRZEJDŹ DALEJ
+        </Link>
+
+        <p className='font-[Jura] text-[20px] font-extralight text-[#E2B350] tracking-[22.5px] bouncingText lg:tracking-[15px] lg:text-center sm:text-[10px] sm:tracking-[8px] fold:w-[90%]'>PASSION MAKES IT BETTER</p>
+      </div>
     </section>
   )
 }
