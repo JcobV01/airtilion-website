fccc7d46557141be682df85b4d2e1614e3c45586
JcobV01 <jakubwadycki@gmail.com>
2024-10-03 09:07:14 +0200

    zmiana obrazka

--- components/home/sections/AboutUs/AboutUs.jsx
+++ components/home/sections/AboutUs/AboutUs.jsx
@@ -23,7 +23,7 @@ const AboutUs = () => {
       <article ref={ref} className={`relative flex gap-[50px] z-10 xl:gap-[30px] lg:gap-[50px] lg:flex-col about-section transition-all duration-1000 ease-in-out ${
         isVisible ? 'about-visible' : 'about-hidden'
       }`}>
-        <Image src={mainImage} width={420} height={290} alt="Obrazek wyróżniający sekcję O nas" className='w-[420px] h-[420px] rounded-[10px] xl:w-[400px] xl:h-[400px] lg:w-full lg:h-[300px] object-cover' />
+        <Image src={mainImage} width={420} height={420} alt="Obrazek wyróżniający sekcję O nas" className='w-[420px] h-[420px] rounded-[10px] xl:w-[400px] xl:h-[400px] lg:w-full lg:h-[300px] object-cover' />
         <div className='flex flex-col gap-[50px]'>
           <Heading title="O nas" subtitle="poznaj naszą firmę" id="about" />
           <p className='text-[16px] font-light xl:text-[15px] lg:text-[13px]'>Jesteśmy zespołem pasjonatów technologii, tworzącym strony i aplikacje internetowe od A do Z, które są dostosowane do Twoich potrzeb. Od wielu lat jesteśmy zafascynowani tą branżą, zauważyliśmy przez ten czas, że coraz więcej firm świadczących podobne usługi staje się coraz bardziej leniwych i skupia się tylko na zyskach, zapominając o jakości i kliencie. Airtilion powstał żeby to zmienić. </p>
