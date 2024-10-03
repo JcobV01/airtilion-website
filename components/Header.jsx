326d1aae5488cbf99542a2405aa67009879694bd
JcobV01 <jakubwadycki@gmail.com>
2024-10-03 12:27:23 +0200

    zdjecie2

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,14 +7,14 @@ const Header = ({ title, subtitle, img }) => {
       <Image
           src={img.src}
           alt="Tło nagłówka"
-          width={1920}
-          height={1080}
+          layout="fill" // Zajmuje cały obszar nagłówka
           objectFit="cover" // Pokrywa cały obszar
           objectPosition="center calc(100% + 10px)" // Pozycjonowanie obrazu
           priority // Ważny obraz
-          className='w-full h-dvh absolute object-cover'
+          className='w-full h-dvh'
           unoptimized={true}
           quality={70}
+
         />
       <article className='flex flex-col items-center justify-center sm:gap-[20px] relative header-border h-[230px] 2xl:h-[180px] md:h-[120px] sm:h-auto px-[120px] 2xl:px-[80px] md:px-[40px] lg:mx-[30px]'>
         <p className='text-[#E2B350] text-[15px] 2xl:text-[13px] lg:text-[10px] font-light lg:text-center'>TWÓJ SUKCES ZACZYNA SIĘ TUTAJ</p>
