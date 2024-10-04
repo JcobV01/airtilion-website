e0cfcc07a911a7a569e47208a075cb39fc806ca5
JcobV01 <jakubwadycki@gmail.com>
2024-10-04 13:52:29 +0200

    test37

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -69,7 +69,7 @@ const home = () => {
 
   return (
     <>
-      <Header title="Budujemy przyszłość Twojej Marki w Internecie" subtitle="Nowoczesne strony i aplikacje internetowe dopasowane do Twoich potrzeb." img={HeaderBgDesktop2} mobile={width > 1023 ? true : false}/>
+      <Header title="Budujemy przyszłość Twojej Marki w Internecie" subtitle="Nowoczesne strony i aplikacje internetowe dopasowane do Twoich potrzeb." img={HeaderBgDesktop} mobile={width > 1023 ? true : false}/>
       <main className='flex flex-col relative mt-[-250px] gap-[200px] md:gap-[100px]'>
         <Encourage />
         <AboutUs />
