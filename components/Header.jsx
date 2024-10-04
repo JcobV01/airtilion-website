7df47be84db678faa89ab51dbd127b536f79c5da
JcobV01 <jakubwadycki@gmail.com>
2024-10-04 17:36:05 +0200

    text-x1

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,7 +19,7 @@ const Header = ({ title, subtitle, img, mobile=true }) => {
           priority
           quality={100}
           fetchPriority="auto"
-          unoptimized={width > 1023 && true}
+          unoptimized={width > 1023}
         />
       <article className='flex flex-col items-center justify-center sm:gap-[20px] relative header-border h-[230px] 2xl:h-[180px] md:h-[120px] sm:h-auto px-[120px] 2xl:px-[80px] md:px-[40px] lg:mx-[30px]'>
         <p className='text-[#E2B350] text-[15px] 2xl:text-[13px] lg:text-[10px] font-light lg:text-center'>TWÓJ SUKCES ZACZYNA SIĘ TUTAJ</p>
