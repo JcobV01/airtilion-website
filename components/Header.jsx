dfbb3b4a05c43c03c6984186a327326264be7bec
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-10-04 18:21:36 +0200

    Testy

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 "use client"
 import useWindowWidth from '@hooks/useWindowWidth'
+import Head from 'next/head'
 import Image from 'next/image'
 import {useEffect, useState} from 'react'
 
@@ -12,28 +13,34 @@ const Header = ({ title, subtitle, img, mobile=true }) => {
   }, [])
   
   return (
+    <>
+    <Head>
+        <link rel="preload" href={img.src} as="image" fetchPriority="high" />
+    </Head>
+
     <header className='w-full h-dvh relative top-[-133px] flex justify-center items-center z-10'>
       {isClient &&
       <Image
-          src={img.src}
-          alt="Tło nagłówka"
-          fill 
-          sizes="(max-width: 1024px) 100vw, 1920px"
-          // sizes="100vw"
-          className='w-full h-dvh absolute object-cover'
-          loading='eager'
-          priority
-          quality={100}
-          fetchPriority="auto"
-          unoptimized={width > 1023}
-        />
-      }
+      src={img.src}
+      alt="Tło nagłówka"
+      fill 
+      sizes="(max-width: 1024px) 100vw, 1920px"
+      // sizes="100vw"
+      className='w-full h-dvh absolute object-cover'
+      loading='eager'
+      priority
+      quality={100}
+      fetchPriority="auto"
+      unoptimized={width > 1023}
+      />
+    }
       <article className='flex flex-col items-center justify-center sm:gap-[20px] relative header-border h-[230px] 2xl:h-[180px] md:h-[120px] sm:h-auto px-[120px] 2xl:px-[80px] md:px-[40px] lg:mx-[30px]'>
         <p className='text-[#E2B350] text-[15px] 2xl:text-[13px] lg:text-[10px] font-light lg:text-center'>TWÓJ SUKCES ZACZYNA SIĘ TUTAJ</p>
         <h1 className='text-[45px] font-semibold 2xl:text-[35px] lg:text-[30px] md:text-[25px] lg:text-center'>{title}</h1>
         <h2 className='text-[#E2B350] text-[20px] 2xl:text-[18px] lg:text-[13px] md:text-[11px] lg:text-center'>{subtitle}</h2>
       </article>
     </header>
+    </>
   )
 }
 
