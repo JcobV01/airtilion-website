9cfc63bddd57bac5b06a3a97ba1dc3bbb176a32a
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-10-05 11:37:01 +0200

    testy

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -77,16 +77,16 @@ const home = () => {
       </Head>
 
       <Header title="Budujemy przyszłość Twojej Marki w Internecie" subtitle="Nowoczesne strony i aplikacje internetowe dopasowane do Twoich potrzeb." img={HeaderBgDesktop} mobile={width > 1023 ? true : false}/>
-      {/* <main className='flex flex-col relative mt-[-250px] gap-[200px] md:gap-[100px]'> */}
-        {/* <Encourage /> */}
-        <AboutUs />
-        {/* <Cooperation />
+      <main className='flex flex-col relative mt-[-250px] gap-[200px] md:gap-[100px]'>
+        <Encourage />
+        {/* <AboutUs /> */}
+        <Cooperation />
         <Offer />
         <Portfolio />
-        <Opinion /> */}
+        <Opinion />
         <Contact />
 
-        {/* <Footer /> */}
+        <Footer />
         {/* <div className='absolute top-0 left-0 w-full h-full background'></div>
 
         {
@@ -95,7 +95,7 @@ const home = () => {
             :
             <MobileHomeLines />
         } */}
-      {/* </main> */}
+      </main>
     </>
   )
 }
