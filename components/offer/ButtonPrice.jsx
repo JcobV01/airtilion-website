357d6fcb040506db2d4a37ea78df389ff82d0429
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-10-05 12:41:43 +0200

    Poprawki w buttonie netto/brutto

--- components/offer/ButtonPrice.jsx
+++ components/offer/ButtonPrice.jsx
@@ -6,7 +6,7 @@ const ButtonPrice = ({isNetto, tooglePrice}) => {
         <div className='flex items-center justify-center gap-[25px] text-[17px] tracking-[1.5px] font-light mt-[60px] xl:mb-[40px]'>
             <span>Netto</span>
             <div>
-                <input type="checkbox" id="toggle" className='sr-only' checked={!isNetto} onChange={tooglePrice} />
+                <input type="checkbox" id="toggle" className='sr-only' checked={!isNetto} onChange={tooglePrice} aria-label="Toggle switch" />
                 <label htmlFor="toggle" className='block w-16 h-8 rounded-[30px] cursor-pointer relative' style={{
                     background: 'radial-gradient(46.61% 50% at 50% 50%, rgba(63, 63, 63, 0.80) 0%, rgba(45, 44, 44, 0.80) 100%)',
                     boxShadow: '5px 5px 4px 0px rgba(0, 0, 0, 0.25)',
