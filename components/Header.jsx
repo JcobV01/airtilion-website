9a01d244885bb440604eba1433f6e22299921c21
JcobV01 <jakubwadycki@gmail.com>
2024-10-05 12:55:03 +0200

    poprawka nagłówków 2

--- components/Header.jsx
+++ components/Header.jsx
@@ -26,6 +26,7 @@ const Header = ({ title, subtitle, img, positionBottom=true }) => {
       quality={100}
       fetchPriority="auto"
       unoptimized={width > 1023}
+      style={{objectPosition: positionBottom == true ? 'center bottom' : 'center center' }}
       />
     }
       <article className='flex flex-col items-center justify-center sm:gap-[20px] relative header-border h-[230px] 2xl:h-[180px] md:h-[120px] sm:h-auto px-[120px] 2xl:px-[80px] md:px-[40px] lg:mx-[30px]'>
