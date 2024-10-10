acdf5632b290acdfcbd03fbf7402a535a843b23e
JcobV01 <jakubwadycki@gmail.com>
2024-10-10 09:35:07 +0200

    optymalizacja headera - próba 3

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,23 @@
 "use client"
+import useIntersectionObserver from '@hooks/useObserver'
 import useWindowWidth from '@hooks/useWindowWidth'
 import Image from 'next/image'
 import {useEffect, useState} from 'react'
 
-const Header = ({ title, subtitle, img, positionBottom=true }) => {
+const Header = ({ title, subtitle, img, positionBottom=true, dialogVisible }) => {
   const width = useWindowWidth()
   const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
     setIsClient(true)
   }, [])
+
+  const [ref, isVisible] = useIntersectionObserver({
+    threshold: 0.1
+  });
   
   return (
-    <header className='w-full h-dvh relative top-[-133px] flex justify-center items-center z-10'>
+    <header ref={ref} className={`w-full h-dvh relative top-[-133px] flex justify-center items-center z-10  transition-opacity duration-1000 ${(isVisible && !dialogVisible) ? 'opacity-100' : 'opacity-0'}`}>
       {isClient &&
       <Image
       src={img.src}
