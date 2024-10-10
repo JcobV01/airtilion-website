b1abfe89a84f86cdfd0d3b934a13558856749689
JcobV01 <jakubwadycki@gmail.com>
2024-10-10 12:14:06 +0200

    optymalizacja headera - próba 8

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = ({ title, subtitle, img, positionBottom=true, dialogVisible }) =>
   
   return (
     <header ref={ref} className={`w-full h-dvh relative top-[-133px] flex justify-center items-center z-10  transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
-      {isClient &&
+      {/* {isClient &&
       <Image
       src={img.src}
       alt="Tło nagłówka"
@@ -33,7 +33,7 @@ const Header = ({ title, subtitle, img, positionBottom=true, dialogVisible }) =>
       unoptimized={width > 1023}
       style={{objectPosition: positionBottom == true ? 'center bottom' : 'center center' }}
       />
-    }
+    } */}
       <article className='flex flex-col items-center justify-center sm:gap-[20px] relative header-border h-[230px] 2xl:h-[180px] md:h-[120px] sm:h-auto px-[120px] 2xl:px-[80px] md:px-[40px] lg:mx-[30px]'>
         <p className='text-[#E2B350] text-[15px] 2xl:text-[13px] lg:text-[10px] font-light lg:text-center'>TWÓJ SUKCES ZACZYNA SIĘ TUTAJ</p>
         <h1 className='text-[45px] font-semibold 2xl:text-[35px] lg:text-[30px] md:text-[25px] lg:text-center'>{title}</h1>
