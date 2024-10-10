fedecde4926f9707333aab57c14c2d62d5747d14
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-10-10 12:48:28 +0200

    Naprawa błędu z domem ikony

--- components/home/sections/Opinion/components/SliderCard.jsx
+++ components/home/sections/Opinion/components/SliderCard.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Icon } from '@iconify/react';
 
-const SliderCard = ({author, page, desc, key}) => {
+const SliderCard = ({ author, page, desc, key }) => {
     return (
         <div className='h-[250px] w-[420px] xxl:h-[300px] xxl:w-[470px] bg-[rgba(16,16,16,0.40)] rounded-[10px] backdrop-blur-[2.5px] px-[15px] py-[15px] relative' key={key}>
             <div className='absolute right-[-20px] top-[-20px]'>
                 <Icon icon="bi:quote" width={200} height={200} alt="Ikona cytatu" className='text-[#121212]' />
             </div>
             <div className='flex gap-[50px] z-10 relative'>
-                <div className='w-[40px] h-[40px] bg-[rgba(22,22,22,0.60)] rounded-[5px] backdrop-blur-[2px] flex justify-center items-center'>
-                    <Icon icon="material-symbols:person" width={25} height={25} alt="Ikona człowieka" className='text-[#E2B350]' />
-                </div>
+                <Icon icon="material-symbols:person" width={25} height={25} alt="Ikona człowieka" className='text-[#E2B350] w-[40px] h-[40px] bg-[rgba(22,22,22,0.60)] rounded-[5px] backdrop-blur-[2px] flex justify-center items-center' />
                 <div className='flex flex-col'>
                     <h5 className='text-[20px] xxl:text-[24px] font-extralight tracking-[1px]'>{author}</h5>
                     <span className='pl-[15px] text-[#AEAEAE] text-[16px] xxl:text-[20px] font-extralight lg:text-[13px]'>{page}</span>
