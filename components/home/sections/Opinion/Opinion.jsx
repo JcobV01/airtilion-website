67d166fa6cc3bf8fb006009c14e0907f6c78282d
JcobV01 <jakubwadycki@gmail.com>
2024-10-10 14:11:26 +0200

    optymalizacja DOM

--- components/home/sections/Opinion/Opinion.jsx
+++ components/home/sections/Opinion/Opinion.jsx
@@ -12,9 +12,9 @@ const Opinion = () => {
     <section ref={ref} className={`w-[1400px] xxl:w-[1500px] 2xl:w-[80%] m-auto flex flex-col gap-[50px] relative z-10 transition-all duration-1000 ease-in-out ${isVisible ? 'about-visible' : 'about-hidden'}`}>
         <Heading title="Opinie" subtitle="POZNAJ OPINIE NASZYCH KLIENTÓW" />
         <p className='text-[15px] xxl:text-[20px] lg:text-[13px]'>Decyzja o wyborze odpowiedniej usługi lub produktu nie jest łatwa. Zanim podejmiesz decyzję, warto dowiedzieć się, co mówią o nas Ci, którzy już skorzystali z naszych usług. Ich rekomendacje to najlepszy dowód na jakość i profesjonalizm, które oferujemy.</p>
-        <article>
+        {/* <article> */}
             <Slider />
-        </article>
+        {/* </article> */}
     </section>
   )
 }
