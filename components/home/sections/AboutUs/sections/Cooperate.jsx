6d1fcbcb3cc2e76f252ff12b1768b1bf891098db
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-10-11 09:15:01 +0200

    Podmiana backgroundImage na image

--- components/home/sections/AboutUs/sections/Cooperate.jsx
+++ components/home/sections/AboutUs/sections/Cooperate.jsx
@@ -2,6 +2,7 @@
 
 import useIntersectionObserver from '@hooks/useObserver';
 import coopBg from '@public/assets/images/cooperate-bg.webp'
+import Image from 'next/image';
 import Link from 'next/link';
 
 
@@ -9,7 +10,8 @@ const Cooperate = () => {
   const [ ref, isVisible ] = useIntersectionObserver();
 
   return (
-    <article ref={ref} className={`w-full h-[450px] lg:h-[350px] sm:h-auto sm:py-[50px] flex flex-col items-center relative px-[170px] xl:px-[100px] lg:px-[50px] sm:px-[30px] gap-[40px] xl:gap-[30px] justify-center bg-center transition-all duration-1000 ease-in-out ${isVisible ? 'about-visible' : 'about-hidden'} xxl:bg-cover xxl:bg-no-repeat`} style={{backgroundImage: `url(${coopBg.src})`}}>
+    <article ref={ref} className={`w-full h-[450px] lg:h-[350px] sm:h-auto sm:py-[50px] flex flex-col items-center relative px-[170px] xl:px-[100px] lg:px-[50px] sm:px-[30px] gap-[40px] xl:gap-[30px] justify-center bg-center transition-all duration-1000 ease-in-out ${isVisible ? 'about-visible' : 'about-hidden'}`}>
+        <Image src={coopBg} alt="Obrazek przedstawiający jednego z założycieli Airtlionu" width="auto" height="450"  className="absolute w-full h-[450px] lg:h-[350px] sm:h-auto object-cover xxl:bg-cover xxl:bg-no-repeat"/>
         <div className='z-10'>
             <p className='z-10 text-[#B8B8B8] lg:text-[13px] font-light text-center'>WSPÓŁPRACUJ Z NAMI</p>
             <h5 className='text-[40px] xl:text-[30px] lg:text-[25px] font-semibold z-10 text-center'>Zawsze jesteśmy gotowi stworzyć <br /> dla Ciebie najlepszy produkt</h5>
