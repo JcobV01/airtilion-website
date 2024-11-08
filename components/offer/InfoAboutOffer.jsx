951824ff9b2c893c85ba1626e3108484f50f5c4b
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-11-08 09:15:53 +0100

    Dodanie checkboxow w dialogu w ofercie + dodanie informacji, ze oferta stanowi charakter informacyjny...

--- components/offer/InfoAboutOffer.jsx
+++ components/offer/InfoAboutOffer.jsx
@@ -9,6 +9,7 @@ const InfoAboutOffer = () => {
     return (
         <div ref={ref} className={`xxl:mt-[150px] transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
             <p className='text-[13px] text-[#ECD8AD] text-center xl:mt-[10px] sm:mt-[20px] xxl:text-[16px]'>*Ceny pakietów nie uwzględniają zakupu domeny oraz hostingu</p>
+            <p className='text-[13px] text-[#EEEEEE] text-center mt-[20px] xxl:text-[16px]'>Przedstawiona oferta cenowa ma charakter informacyjny i nie stanowi oferty handlowej w rozumieniu Art.66 par.1 Kodeksu Cywilnego.<br />Podane informacje stanowią jedynie zaproszenie do składania ofert i zawarcia umowy określone w art. 71 Kodeksu cywilnego</p>
             <p className='text-[18px] text-center font-light mt-[40px] xxl:text-[21px] xl:w-[90%] xl:mx-auto sm:text-[16px] fold:text-[14px]'>Nie znalazłeś oferty idealnie dopasowanej do Twoich potrzeb? Nie martw się! Kontynuując, masz możliwość dostosowania usługi do własnych wymagań w zakładce <Link href="/offer/pricing" className="text-[#E2B350] duration-700 inline-flex hover:scale-90">“Darmowa wycena”</Link>. Wybierz to, co najlepiej odpowiada Twoim oczekiwaniom i stwórz usługę, która będzie spełniała wszystkie Twoje potrzeby.</p>
             <div className='flex justify-center mt-[40px]'>
                 <hr className='w-[900px] bg-[rgba(226,179,80,0.50)] h-[1px] border-none xl:w-[80%] xxl:w-[1200px]'></hr>
