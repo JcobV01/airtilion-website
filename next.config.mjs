ffc487c8b3686c7d2d0787e543a432db95c69196
JcobV01 <jakubwadycki@gmail.com>
2024-12-30 08:59:23 +0100

    nowa konfiguracja

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compress: false
+    compress: true,
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // Dotyczy wszystkich ścieżek
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: `
+                            default-src 'self';
+                            script-src 'self' 'unsafe-eval';
+                            style-src 'self';
+                            font-src 'self' fonts.gstatic.com;
+                            img-src 'self' data:;
+                            connect-src 'self';
+                            object-src 'none';
+                            base-uri 'self';
+                        `.replace(/\s{2,}/g, " ").trim()
+                    }
+                ]
+            }
+        ];
+    }
 };
+
 export default nextConfig;
