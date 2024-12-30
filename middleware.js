3d3fd0d41024a7534717de4b6b2a01ccdd268be6
JcobV01 <jakubwadycki@gmail.com>
2024-12-30 09:28:26 +0100

    powrót do default styles

--- middleware.js
+++ middleware.js
@@ -8,20 +8,6 @@ export function middleware(req) {
   const response = NextResponse.next();
   response.headers.set('x-current-path', urlPath);
 
-  //nagłówki dla CSP
-
-  const nonce = crypto.randomBytes(16).toString("base64");
-  req.headers.set("Content-Security-Policy", `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval';
-    style-src 'self' 'nonce-${nonce}';
-    font-src 'self' fonts.gstatic.com;
-    img-src 'self' data:;
-    connect-src 'self';
-    object-src 'none';
-    base-uri 'self';
-  `.replace(/\s{2,}/g, " ").trim());
-
   return response;
 }
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compress: true,
-    async headers() {
-        return [
-            {
-                source: "/(.*)", // Dotyczy wszystkich ścieżek
-                headers: [
-                    {
-                        key: "Content-Security-Policy",
-                        value: `
-                            default-src 'self';
-                            script-src 'self' 'unsafe-eval';
-                            style-src 'self';
-                            font-src 'self' fonts.gstatic.com;
-                            img-src 'self' data:;
-                            connect-src 'self';
-                            object-src 'none';
-                            base-uri 'self';
-                        `.replace(/\s{2,}/g, " ").trim()
-                    }
-                ]
-            }
-        ];
-    }
+    compress: true
 };
-
 export default nextConfig;
