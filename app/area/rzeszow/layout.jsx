aad90a221b517c4a128122d8bb7cb82d60f11a3c
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-04-14 12:43:12 +0200

    SEO Rzeszów

--- app/area/rzeszow/layout.jsx
+++ app/area/rzeszow/layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const metadata = {
-    title: "Airtilion - Strony internetowe i aplikacje Rzeszów",
-    description: "Nowoczesne strony internetowe, sklepy online, projekty graficzne i aplikacje mobilne. Działamy w Rzeszowie i okolicach - sprawdź naszą ofertę!",
+    title: "Strony internetowe Tarnów - Airtilion",
+    description: "Nowoczesne strony internetowe w Rzeszowie – projektujemy strony, sklepy online, grafiki oraz aplikacje mobilne. Działamy w Rzeszowie i okolicach – sprawdź ofertę!",
     alternates: {
         canonical: 'https://airtilion.com/area/rzeszow',
     },
--- app/area/rzeszow/page.jsx
+++ app/area/rzeszow/page.jsx
@@ -1,10 +1,191 @@
 import React from 'react'
-import CityComponent from '@components/area/CityComponent'
 
+import CityComponent from '@components/area/CityComponent'
+import WhyCity from '@components/area/WhyCity'
+import SliderSection from '@components/area/Slider'
+import ProcessCity from '@components/area/ProcessCity'
+import OfferCity from '@components/area/OfferCity'
+import CityPortfolio from '@components/area/CityPortfolio'
+import CityFaq from '@components/area/faq/CityFaq'
+import Cooperate from '@components/home/sections/AboutUs/sections/Cooperate'
+
+import rzeszow from '@public/assets/images/area/cities/rzeszow.webp'
+import firstCard from '@public/assets/images/area/offer/card-1.webp'
+import secondCard from '@public/assets/images/area/offer/card-2.webp'
+import thirdCard from '@public/assets/images/area/offer/card-3.webp'
+import fourthCard from '@public/assets/images/area/offer/card-4.webp'
+import fifthCard from '@public/assets/images/area/offer/card-5.webp'
+import firstIllustration from '@public/assets/images/area/offer/illustration-1.svg'
+import secondllustration from '@public/assets/images/area/offer/illustration-2.svg'
+import thirdllustration from '@public/assets/images/area/offer/illustration-3.svg'
+
+const cityData = {
+  firstSection: {
+    title: "Strony internetowe Rzeszów",
+    content: "Twoja firma zasługuje na stronę, która przyciąga klientów i dominuje w Google! Tworzymy nowoczesne strony internetowe w Rzeszowie – szybkie, responsywne i zoptymalizowane pod SEO. Postaw na jakość i technologię przyszłości!",
+    tiles: {
+      title: "Dlaczego warto zainwestować w stronę internetową w Rzeszowie?",
+      image: rzeszow,
+      elements: [
+        {
+          icon: "gis:location-poi",
+          iconText: "ZASIĘG LOKALNY",
+          title: "Zdobędziesz klientów w Rzeszowie i okolicach",
+          desc: "Zwiększ zasięg swojego biznesu i przyciągnij nowych klientów z Rzeszowa"
+        },
+        {
+          icon: "material-symbols:trophy",
+          iconText: "PRZEWAGA NAD KONKURENCJĄ",
+          title: "Wyróżnisz się na tle konkurencji",
+          desc: "Profesjonalne strony przyciągają nowych klientów z Rzeszowa i okolic"
+        },
+        {
+          icon: "fa6-solid:handshake-simple",
+          iconText: "WIARYGODNOŚĆ MARKI",
+          title: "Zbudujesz zaufanie i wiarygodność swojej marki",
+          desc: "Strona internetowa to w dzisiejszych czasach nieodłączna wizytówka"
+        },
+        {
+          icon: "icomoon-free:stats-dots",
+          iconText: "WIĘCEJ KLIENTÓW",
+          title: "Zwiększysz sprzedaż i liczbę zapytań ofertowych",
+          desc: "Skuteczna strona internetowa to więcej zapytań i większa sprzedaż"
+        }
+      ]
+    }
+  },
+  technologies: {
+    title: "TECHNOLOGIE",
+    desc: "Tworzymy funkcjonalne, estetyczne i responsywne strony internetowe w Rzeszowie oparte o nowoczesne technologie, które zapewniają niezawodność, szybkość i łatwość użytkowania. Nasze projekty są zaprojektowane z myślą o użytkownikach, dzięki czemu Twoja strona będzie nie tylko piękna, ale również intuicyjna i łatwa w nawigacji. Wykorzystujemy najnowsze technologie, które pozwalają na szybkie ładowanie stron, lepszą optymalizację pod kątem SEO oraz pełną responsywność.",
+    techs: [
+      {
+        name: "TypeScript",
+        slug: ["BEZPIECZEŃSTWO", "PRZEJRZYSTOŚĆ"],
+        icon: "akar-icons:typescript-fill"
+      },
+      {
+        name: "NextJS",
+        slug: ["ELASTYCZNOŚĆ", "SKALOWALNOŚĆ"],
+        icon: "akar-icons:nextjs-fill"
+      },
+      {
+        name: "ReactJS",
+        slug: ["SZYBKOŚĆ", "WYDAJNOŚĆ"],
+        icon: "mdi:react"
+      },
+      {
+        name: "Wordpress",
+        slug: ["POPULARNOŚĆ", "PROSTOTA"],
+        icon: "ic:baseline-wordpress"
+      },
+      {
+        name: "Tailwind",
+        slug: ["MINIMALIZM", "SWOBODA"],
+        icon: "mdi:tailwind"
+      }
+    ]
+  },
+  process: {
+    title: "PROCES TWORZENIA STRON INTERNETOWYCH W RZESZOWIE",
+    desc: "Tworzenie stron internetowych w Rzeszowie",
+    question: "Chcesz stworzyć profesjonalną stronę internetową w Rzeszowie?",
+    elements: [
+      {
+        icon: "mdi:clipboard-text",
+        title: "Analiza potrzeb klienta",
+        desc: "Na początku poznajemy specyfikę Twojej działalności oraz cele biznesowe, aby stworzyć stronę internetową w Rzeszowie dopasowaną do Twoich oczekiwań."
+      },
+      {
+        icon: "solar:pallete-2-bold",
+        title: "Projekt graficzny",
+        desc: "Projektujemy nowoczesne, responsywne i przyciągające uwagę layouty, które wyróżnią Twoją firmę. Każda strona internetowa jest tworzona z myślą o przejrzystości i wygodzie użytkownika."
+      },
+      {
+        icon: "fa6-solid:laptop-code",
+        title: "Programowanie",
+        desc: "Wdrażamy stronę z użyciem najnowszych technologii. Dbamy o szybkość działania i bezpieczeństwo strony, zapewniając pełną funkcjonalność."
+      },
+      {
+        icon: "material-symbols:rocket",
+        title: "Wdrożenie i wsparcie",
+        desc: "Publikujemy gotową stronę w Rzeszowie i oferujemy pomoc techniczną po jej uruchomieniu, aby wszystko działało sprawnie od samego początku."
+      },
+    ],
+  },
+  offer: {
+    desc: "Tworzymy nowoczesne, szybkie i przyjazne SEO strony internetowe, które przyciągną klientów z Rzeszowa i okolic. Specjalizujemy się w:",
+    elements: [
+      {
+        title: "Strony wizytówki",
+        desc: "Idealne dla małych firm i freelancerów z Rzeszowa, które chcą zaprezentować się online.",
+        bg: firstCard,
+        illustration: firstIllustration
+      },
+      {
+        title: "Strony firmowe",
+        desc: "Kompleksowe rozwiązania dla przedsiębiorstw, wspierające wizerunek i sprzedaż w regionie.",
+        bg: secondCard,
+        illustration: secondllustration
+      },
+      {
+        title: "Sklepy internetowe",
+        desc: "Bezpieczne i funkcjonalne sklepy dostosowane do potrzeb lokalnego rynku.",
+        bg: thirdCard,
+        illustration: thirdllustration
+      },
+      {
+        title: "Landing page",
+        desc: "Skuteczne strony docelowe, które zwiększą konwersję Twoich kampanii reklamowych.",
+        bg: fourthCard
+      },
+      {
+        title: "Darmowa wycena w 48h!",
+        desc: "Masz pytania? Chcesz poznać koszt strony dostosowanej do Twoich potrzeb? Wypełnij formularz, a przygotujemy dla Ciebie spersonalizowaną ofertę dla strony internetowej w Rzeszowie!",
+        bg: fifthCard
+      },
+    ],
+  },
+  accordion: {
+    elements: [
+      {
+        id: 1,
+        question: "Jak długo trwa stworzenie strony internetowej w Rzeszowie?",
+        answer: "Czas realizacji zależy od skomplikowania projektu oraz Twoich indywidualnych wymagań. Standardowo proces ten trwa od kilku tygodni do kilku miesięcy. Po wstępnej analizie potrzeb możemy przedstawić dokładniejszy harmonogram."
+      },
+      {
+        id: 2,
+        question: "Ile kosztuje strona internetowa w Rzeszowie?",
+        answer: "Koszt zależy od wielu czynników, takich jak zakres funkcjonalności, stopień skomplikowania projektu oraz dodatkowe usługi. Po zapoznaniu się z Twoimi wymaganiami przygotujemy indywidualną wycenę."
+      },
+      {
+        id: 3,
+        question: "Czy będę mógł samodzielnie zarządzać treścią na mojej stronie?",
+        answer: "Tak, nasze strony są wyposażone w intuicyjne systemy zarządzania treścią (CMS), które pozwalają na łatwe dodawanie i edytowanie treści bez potrzeby posiadania wiedzy programistycznej."
+      },
+      {
+        id: 4,
+        question: "Jakie informacje są potrzebne do rozpoczęcia współpracy?",
+        answer: "Na początek potrzebujemy poznać Twoje oczekiwania i cele związane ze stroną, informacje o Twojej firmie, preferencje dotyczące designu oraz funkcjonalności, a także ewentualne materiały, takie jak logo czy treści."
+      },
+    ]
+  }
+}
 
 const page = () => {
   return (
-      <CityComponent city="w Rzeszowie"/>
+    <>
+      <CityComponent city="w Rzeszowie" />
+      <WhyCity data={cityData.firstSection} />
+      <SliderSection data={cityData.technologies} />
+      <ProcessCity data={cityData.process} />
+      <OfferCity data={cityData.offer} />
+      <CityPortfolio city="w Rzeszowie" />
+      <CityFaq data={cityData.accordion} />
+      <section className='w-[1240px] 2xl:w-[960px] lg:w-[90%] mx-auto'>
+        <Cooperate />
+
+      </section>
+    </>
   )
 }
 
