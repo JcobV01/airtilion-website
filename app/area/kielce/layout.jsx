36b1c75b824b1cae85df38703f667ecf8b42d798
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-04-14 13:26:12 +0200

    SEO Kielce i Kraków

--- app/area/kielce/layout.jsx
+++ app/area/kielce/layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const metadata = {
-    title: "Airtilion - Strony internetowe i aplikacje Kielce",
-    description: "Nowoczesne strony internetowe, sklepy online, projekty graficzne i aplikacje mobilne. Działamy w Kielcach i okolicach - sprawdź naszą ofertę!",
+    title: "Strony internetowe Kielce - Airtilion",
+    description: "Nowoczesne strony internetowe w Kielcach – projektujemy strony, sklepy online, grafiki oraz aplikacje mobilne. Działamy w Kielcach i okolicach – sprawdź ofertę!",
     alternates: {
         canonical: 'https://airtilion.com/area/kielce',
     },
