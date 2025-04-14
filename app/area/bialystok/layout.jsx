be8cd35af40abe8c96e2c4f40397902f67d91988
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-04-14 13:46:09 +0200

    SEO Białystok

--- app/area/bialystok/layout.jsx
+++ app/area/bialystok/layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const metadata = {
-    title: "Airtilion - Strony internetowe i aplikacje Białystok",
-    description: "Nowoczesne strony internetowe, sklepy online, projekty graficzne i aplikacje mobilne. Działamy w Białymstoku i okolicach - sprawdź naszą ofertę!",
+    title: "Strony internetowe Białystok - Airtilion",
+    description: "Nowoczesne strony internetowe w Białymstoku – projektujemy strony, sklepy online, grafiki oraz aplikacje mobilne. Działamy w Białymstoku i okolicach – sprawdź ofertę!",
     alternates: {
         canonical: 'https://airtilion.com/area/bialystok',
     },
