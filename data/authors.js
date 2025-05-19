fa66fc322d554ecffdc4c07e83771c2b7c1e1ace
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-05-19 08:12:40 +0200

    Zmiana opisu

--- data/authors.js
+++ data/authors.js
@@ -5,7 +5,7 @@ export const authors = [
     {
         name: 'Artur Plebańczyk',
         profession: 'Współwłaściciel Airtilion Sp. z o.o.',
-        bio: 'Ekspert od wycen dedykowanych rozwiązań i zarządzania projektami. Posiada ogromne doświadczenie w tworzeniu ofert idealnie dopasowanych do potrzeb i oczekiwań klientów, specjalista łączący pasję do nowych wyzwań z analitycznym podejściem do każdego szczegółu projektu.',
+        bio: 'Ekspert wdrażania dedykowanych rozwiązań IT oraz zarządzania projektami. Posiada kilkuletnie doświadczenie w opracowywaniu i realizacji rozwiązań idealnie dopasowanych do indywidualnych potrzeb klientów. Pasjonat nowych technologii. Analityczne podejście umożliwia precyzyjną kontrolę każdego etapu projektu, gwarantując terminowość i najwyższą jakość.',
         image: authorImg2
     },
     {
