6bb583493612544d80ddc7e0c7caa9034df74ad5
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-05-20 10:48:57 +0200

    Testowo dodane fetchpriority high

--- components/blog-singlePost/ArticleIntro.jsx
+++ components/blog-singlePost/ArticleIntro.jsx
@@ -6,7 +6,7 @@ const ArticleIntro = ({ title, image_link, intro_text }) => {
         <section className='mt-[64px] w-[1240px] mx-auto flex gap-[32px] xl:w-[1000px] lg:w-[90%] lg:flex-col lg:items-center'>
 
             <div className='flex-1 relative h-full'>
-                <Image src={image_link} alt={title.replace(/<[^>]+>/g, '').replace(/&#8211;/g, '-')} loading='eager' width="600" height="400" sizes={`(min-width: 1280px) 50vw, (min-width: 1024px) 484px, (min-width: 768px) 50vw, 100%`} className='rounded-[5px] object-cover' />
+                <Image src={image_link} alt={title.replace(/<[^>]+>/g, '').replace(/&#8211;/g, '-')} loading='eager' fetchPriority="high" width="600" height="400" sizes={`(min-width: 1280px) 50vw, (min-width: 1024px) 484px, (min-width: 768px) 50vw, 100%`} className='rounded-[5px] object-cover' />
                 <div className='rounded-[5px] bg-[linear-gradient(202deg,#E2B35000_60%,#E2B350ff_140%)] brightness-50 absolute top-0 left-0 w-[600px] h-full xl:w-[484px] lg:w-full' />
             </div>
 
