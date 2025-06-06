3311251dcd3e0449831e19436d59a8fc6dd4e40b
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-06-06 13:20:43 +0200

    SEO dla Kielc

--- components/area/ProcessCity.jsx
+++ components/area/ProcessCity.jsx
@@ -17,7 +17,7 @@ const ProcessCity = ({ processCity, city }) => {
                     <p className='mt-[16px] font-extralight text-[18px] sm:text-[16px]'>{processCity.paragraph1}</p>
                     <p className='mt-[48px] text-[16px] font-extralight sm:text-[14px]'>{processCity.cta}</p>
                 </div>
-                <div className={`absolute left-0 ${city === 'Lublin' ? 'bottom-[245px] xl:bottom-[220px]' : 'bottom-[150px] xl:bottom-[90px]'}  w-[240px]  lg:relative lg:mt-[16px] lg:bottom-0 lg:self-start`}>
+                <div className={`absolute left-0 ${city === 'Lublin' ? 'bottom-[245px] xl:bottom-[220px]' : city === 'Kielce' ? 'bottom-[105px] xl:bottom-[70px]' : 'bottom-[150px] xl:bottom-[90px]'}  w-[240px]  lg:relative lg:mt-[16px] lg:bottom-0 lg:self-start`}>
                     <CustomContact text="Skontaktuj się z nami" width={240} />
                 </div>
                 <div className='mt-[32px] w-[600px] flex gap-x-[100px] gap-y-[40px] flex-wrap xl:gap-x-[40px] lg:mt-[64px] lg:justify-center lg:gap-x-[100px] sm:w-full sm:flex-col sm:items-center'>
--- data/cities/kielce.js
+++ data/cities/kielce.js
@@ -3,27 +3,27 @@ import kielceImg from '@public/assets/images/area/cities/kielce.webp'
 export const kielce = {
     city: "Kielce",
     img: kielceImg,
-    metaTitle: "Strony www Kielce - projektowanie, tworzenie, sklepy internetowe, optymalizacja",
-    metaDescription: "Projektujemy i tworzymy profesjonalne strony www Kielce oraz sklepy internetowe z CMS i kompleksową optymalizacją SEO. Sprawdź ofertę!",
+    metaTitle: "Strony www Kielce - projektowanie, tworzenie, sklepy internetowe",
+    metaDescription: "Tworzymy responsywne strony www i sklepy internetowe dla firm z Kielc. Oferujemy pomoc w doborze hostingu i domeny. Projektujemy i tworzymy strony www zoptymalizowane pod SEO.",
 
     header: {
         h1: "Strony internetowe Kielce",
-        paragraph1: "Tworzymy nowoczesne i eleganckie strony internetowe dopasowane do potrzeb klientów z regionu, łącząc funkcjonalność z estetyką. Oferujemy projektowanie i tworzenie stron internetowych od zera, korzystając z intuicyjnego systemu CMS.",
-        paragraph2: "Każda strona www Kielce jest w pełni responsywna, szybka i bezpieczna. Obsługujemy klientów z regionu oraz z całej Polski - nie mamy ograniczeń geograficznych. Skontaktuj się z nami, a omówimy szczegóły współpracy, opowiemy o procesie tworzenia strony i przedstawimy darmową wycenę!"
+        paragraph1: "Tworzymy nowoczesne, eleganckie i responsywne strony dla firm z różnych branż, dopasowane do potrzeb klientów z regionu. Oferujemy projektowanie i tworzenie stron www od zera, korzystając z intuicyjnego systemu CMS lub dedykowanych rozwiązań. Zapewniamy także pomoc w doborze hostingu i domeny.",
+        paragraph2: "Każda strona www Kielce jest w pełni responsywna, szybka i bezpieczna. Obsługujemy klientów z regionu oraz z całej Polski - nie mamy ograniczeń geograficznych. Wszystkie strony tworzone są z myślą o urządzeniach mobilnych, co poprawia ruch z wyszukiwarki Google. Skontaktuj się z nami, a omówimy szczegóły współpracy, opowiemy o procesie tworzenia strony i przedstawimy darmową wycenę!"
     },
 
     whyCity: {
         mainTitle: "Profesjonalne strony i sklepy internetowe Kielce",
         decoration: "Z nami zyskujesz więcej niż tylko obecność w sieci",
         subTitle: "Projektowanie stron internetowych Kielce - nowoczesność i jakość w jednym miejscu",
-        paragraph: "Twoja firma potrzebuje profesjonalnej strony www, która przyciąga potencjalnych klientów i osiąga wyższe pozycje w wyszukiwarce. Projektujemy nowoczesne strony zgodnie z obecnymi trendami - szybkie, responsywne i zoptymalizowane pod SEO. Postaw na jakość i technologię przyszłości!",
+        paragraph: "Twoja firma potrzebuje profesjonalnej strony www, która przyciąga potencjalnych klientów i osiąga wyższe pozycje w wyszukiwarce? Projektujemy nowoczesne strony zgodnie z obecnymi trendami - szybkie, responsywne i zoptymalizowane pod SEO. Postaw na jakość i technologię przyszłości!",
         titleWhyInvest: "Dlaczego warto zainwestować w stronę internetową dla firmy w Kielcach?",
         elements: [
             {
                 icon: "material-symbols:trophy",
                 iconText: "PRZEWAGA NAD KONKURENCJĄ",
                 title: "Wyróżnisz się na tle konkurencji dzięki optymalizacji SEO",
-                desc: `Profesjonalna witryna z odpowiednim linkowaniem i optymalizacją SEO sprawi, że Twoja marka zyska większe pozycje w wynikach wyszukiwania. Dzięki temu wyróżnisz się wśród konkurencji, a klienci szybciej trafią na Twoją ofertę.`
+                desc: `Profesjonalna witryna z odpowiednim linkowaniem, pozycjonowaniem lokalnym i optymalizacją SEO sprawi, że Twoja strona www zyska większe pozycje w wynikach wyszukiwania. Dzięki temu wyróżnisz się wśród konkurencji, a klienci szybciej trafią na Twoją ofertę.`
             },
             {
                 icon: "fa6-solid:handshake-simple",
@@ -41,20 +41,20 @@ export const kielce = {
                 icon: "gis:location-poi",
                 iconText: "ZASIĘG LOKALNY",
                 title: `Zdobędziesz klientów w Kielcach i okolicach`,
-                desc: `Zwiększ zasięg swojego biznesu i przyciągnij nowych klientów z Kielc.`
+                desc: `Zwiększ zasięg swojego biznesu i przyciągnij nowych klientów z Kielc i okolic.`
             }
         ]
     },
 
     technology: {
-        title: "Bezpieczne systemy i&nbsp;technologie dla stron www w Kielcach",
-        paragraph1: "Tworzymy funkcjonalne, estetyczne i w pełni responsywne witryny oparte na nowoczesnych technologiach, które gwarantują niezawodność, szybkość działania i bezproblemowe funkcjonowanie strony w każdych warunkach. Nasze projekty stron powstają z myślą o indywidualnych potrzebach klienta - Twoja witryna będzie nie tylko atrakcyjna wizualnie, ale przede wszystkim intuicyjna w obsłudze.",
-        paragraph2: "Wykorzystujemy rozwiązania przyspieszające ładowanie (minifikacja kodu, kompresja obrazów), dbamy o optymalizację SEO oraz w razie potrzeby implementujemy system CMS, dzięki któremu będziesz mógł/mogła samodzielnie edytować swoją stronę www. Dzięki temu wszystko działa płynnie na komputerach i urządzeniach mobilnych, a regularne aktualizacje i backupy chronią dane osobowe użytkowników."
+        title: "Technologie dla stron www Kielce",
+        paragraph1: "Tworzymy funkcjonalne, estetyczne i w pełni responsywne strony (RWD) oparte na nowoczesnych technologiach, które gwarantują niezawodność, szybkość działania i bezproblemowe funkcjonowanie strony w każdych warunkach. Nasze projekty stron powstają z myślą o indywidualnych potrzebach klienta - Twoja strona internetowa będzie nie tylko atrakcyjna wizualnie, ale przede wszystkim intuicyjna w obsłudze.",
+        paragraph2: "Wykorzystujemy rozwiązania przyspieszające ładowanie (minifikacja kodu, kompresja obrazów), dbamy o optymalizację SEO oraz w razie potrzeby implementujemy system CMS, dzięki któremu będziesz mógł/mogła samodzielnie edytować swoją stronę internetową. Dzięki temu wszystko działa płynnie na komputerach i urządzeniach mobilnych, a regularne aktualizacje i backupy chronią dane osobowe użytkowników."
     },
 
     processCity: {
-        title: "Proces projektowania i&nbsp;tworzenia stron www dla firm w Kielcach",
-        paragraph1: "Proces tworzenia witryny dla firm w regionie to dla nas nie tylko kodowanie, lecz także budowanie silnej obecności online. Każdy etap realizacji jest dokładnie przemyślany, aby dostarczyć rozwiązanie spełniające oczekiwania Twoich klientów i użytkowników.",
+        title: "Proces projektowania i&nbsp;tworzenia responsywnych stron stron internetowych dla firm w Kielcach",
+        paragraph1: "Proces tworzenia witryny dla firm w regionie to dla nas nie tylko kodowanie, lecz także budowanie silnej obecności online. Dbamy o indywidualne podejście do klienta, a każdy etap realizacji jest dokładnie przemyślany, aby dostarczyć rozwiązanie spełniające oczekiwania Twoich klientów i użytkowników.",
         cta: "Chcesz zlecić budowę profesjonalnej strony www?",
         elements: [
             {
@@ -75,20 +75,20 @@ export const kielce = {
             {
                 icon: "material-symbols:rocket",
                 title: "Wdrożenie i aktualizacje",
-                desc: "Publikujemy gotową stronę i oferujemy wsparcie techniczne po jej uruchomieniu, aby wszystko działało sprawnie od samego początku."
+                desc: "Publikujemy gotową stronę na wskazanym serwerze i oferujemy wsparcie techniczne po jej uruchomieniu, aby wszystko działało sprawnie od samego początku."
             },
         ]
     },
 
     offerCity: {
         title: "Nasza oferta strony www dla firm z Kielc",
-        paragraph1: "Tworzymy nowoczesne, szybkie i przyjazne SEO strony internetowe, które przyciągną klientów z Kielc i okolic. Specjalizujemy się w:",
+        paragraph1: "Jako doświadczona firma zajmująca się tworzeniem stron internetowych, oferujemy pełny zakres kompleksowych usług dla firm w sieci, obejmujący projektowanie, wdrożenia oraz opiekę nad stronami. Dbamy o responsywność strony na wszystkich urządzeniach. Nasze rozwiązania sprawią, że Twoja strona stanie się prawdziwą wizytówką Twojej firmy online. Specjalizujemy się w:",
     },
 
     portfolio: {
         title: "Chcesz poznać nasze realizacje?",
         paragraph1: "Zobacz wybrane realizacje i przekonaj się, jak możemy pomóc rozwinąć Twoją działalność online.",
-        paragraph2: "Masz pomysł na stronę internetową w Kielcach?"
+        paragraph2: "Masz pomysł na witrynę internetową w Kielcach?"
     },
 
     faq: {
@@ -97,8 +97,8 @@ export const kielce = {
         elements: [
             {
                 id: 1,
-                question: "Jak długo trwa stworzenie witryny?",
-                answer: "Czas realizacji zależy od skomplikowania projektu oraz Twoich indywidualnych wymagań. Standardowo proces ten trwa od kilku tygodni do kilku miesięcy. Po wstępnej analizie potrzeb możemy przedstawić dokładniejszy harmonogram."
+                question: "Jak długo trwa stworzenie nowoczesnej i funkcjonalnej strony internetowej?",
+                answer: "Czas realizacji zależy od skomplikowania projektu strony www oraz Twoich indywidualnych wymagań. Standardowo proces ten trwa od kilku tygodni do kilku miesięcy. Po wstępnej analizie potrzeb możemy przedstawić dokładniejszy harmonogram."
             },
             {
                 id: 2,
@@ -118,7 +118,7 @@ export const kielce = {
             {
                 id: 5,
                 question: "Co mogę zyskać z dobrze wykonanej strony?",
-                answer: "Dobrze wykonana strona internetowa Twojej firmy pozwala wyróżnić się na tle konkurencji. Dzięki stronie internetowej zyskasz unikalny design, a odpowiednie SEO strony www zapewni lepszą widoczność w wyszukiwarce i przyciągnie więcej potencjalnych klientów."
+                answer: "Dzięki nowej stronie internetowej wyróżnisz się na tle konkurencji, zyskasz unikalny design, a odpowiednie SEO strony www zapewni lepszą widoczność w wyszukiwarce i przyciągnie więcej potencjalnych klientów w Twojej okolicy i nie tylko."
             },
         ]
     }
