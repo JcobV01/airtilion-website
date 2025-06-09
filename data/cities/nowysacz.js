25b1e842235bdb43795f44e2b97f221c5b666d61
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-06-09 10:48:54 +0200

    SEO dla Nowego Sącza

--- data/cities/nowysacz.js
+++ data/cities/nowysacz.js
@@ -3,32 +3,32 @@ import nowysaczImg from '@public/assets/images/area/cities/nowysacz.webp'
 export const nowysacz = {
     city: "Nowy Sącz",
     img: nowysaczImg,
-    metaTitle: "Strony www Nowy Sącz - projektowanie, tworzenie, sklepy internetowe, optymalizacja",
-    metaDescription: "Projektujemy i tworzymy profesjonalne strony www Nowy Sącz oraz sklepy internetowe z CMS i kompleksową optymalizacją SEO. Sprawdź ofertę!",
+    metaTitle: "Projektowanie i tworzenie stron internetowych Nowy Sącz",
+    metaDescription: "Projektujemy i tworzymy profesjonalne strony internetowe Nowy Sącz oraz sklepy internetowe z CMS i kompleksową optymalizacją SEO. Sprawdź ofertę!",
 
     header: {
-        h1: "Strony internetowe Nowy Sącz",
-        paragraph1: "Tworzymy nowoczesne i eleganckie strony internetowe dopasowane do potrzeb klientów z regionu, łącząc funkcjonalność z estetyką. Oferujemy projektowanie i tworzenie stron internetowych od zera, korzystając z intuicyjnego systemu CMS.",
-        paragraph2: "Każda strona www Nowy Sącz jest w pełni responsywna, szybka i bezpieczna. Obsługujemy klientów z regionu oraz z całej Polski - nie mamy ograniczeń geograficznych. Skontaktuj się z nami, a omówimy szczegóły współpracy, opowiemy o procesie tworzenia strony i przedstawimy darmową wycenę!"
+        h1: "Projektowanie i tworzenie stron internetowych Nowy Sącz",
+        paragraph1: "Tworzymy nowoczesne, eleganckie strony internetowe dopasowane do potrzeb klientów z regionu, łącząc funkcjonalność z estetyką. Oferujemy projektowanie i tworzenie stron internetowych od zera, korzystając z intuicyjnego systemu CMS lub dedykowanych rozwiązań. Zapewniamy także pomoc w doborze hostingu i domeny.",
+        paragraph2: "Każda strona jest w pełni responsywna, szybka i bezpieczna (z certyfikatem SSL). Obsługujemy klientów z regionu oraz z całej Polski - nie mamy ograniczeń geograficznych. Skontaktuj się z nami - omówimy szczegóły współpracy, opowiemy o procesie tworzenia sklepów internetowych oraz stron www, a także przygotujemy dla Ciebie darmową wycenę!"
     },
 
     whyCity: {
         mainTitle: "Profesjonalne strony i sklepy internetowe Nowy Sącz",
         decoration: "Z nami zyskujesz więcej niż tylko obecność w sieci",
         subTitle: "Projektowanie stron internetowych Nowy Sącz - nowoczesność i jakość w jednym miejscu",
-        paragraph: "Twoja firma potrzebuje profesjonalnej strony www, która przyciąga potencjalnych klientów i osiąga wyższe pozycje w wyszukiwarce. Projektujemy nowoczesne strony zgodnie z obecnymi trendami - szybkie, responsywne i zoptymalizowane pod SEO. Postaw na jakość i technologię przyszłości!",
+        paragraph: "Twoja firma potrzebuje profesjonalnej strony www, która przyciąga potencjalnych klientów i osiąga wyższe pozycje w wyszukiwarce? Projektujemy nowoczesne strony internetowe zgodnie z obecnymi trendami - szybkie, responsywne i zoptymalizowane pod SEO. Postaw na jakość i technologię przyszłości!",
         titleWhyInvest: "Dlaczego warto zainwestować w stronę internetową dla firmy w Nowym Sączu?",
         elements: [
             {
                 icon: "material-symbols:trophy",
                 iconText: "PRZEWAGA NAD KONKURENCJĄ",
                 title: "Wyróżnisz się na tle konkurencji dzięki optymalizacji SEO",
-                desc: `Profesjonalna witryna z odpowiednim linkowaniem i optymalizacją SEO sprawi, że Twoja marka zyska większe pozycje w wynikach wyszukiwania. Dzięki temu wyróżnisz się wśród konkurencji, a klienci szybciej trafią na Twoją ofertę.`
+                desc: "Profesjonalna strona z odpowiednim linkowaniem i optymalizacją SEO sprawi, że Twoja marka zyska większe pozycje w wynikach wyszukiwania. Dzięki temu wyróżnisz się wśród konkurencji, a klienci szybciej trafią na Twoją ofertę."
             },
             {
                 icon: "fa6-solid:handshake-simple",
                 iconText: "WIARYGODNOŚĆ MARKI",
-                title: `Zbudujesz zaufanie i wiarygodność Twojej firmy z Nowego Sącza`,
+                title: "Zbudujesz zaufanie i wiarygodność Twojej firmy z Nowego Sącza",
                 desc: "Własna strona www to dziś nieodłączna wizytówka. Estetyczny wygląd, przejrzysty układ treści oraz sekcja z referencjami i opiniami budują zaufanie wśród użytkowników i pozycjonują Twoją firmę jako eksperta w branży."
             },
             {
@@ -40,20 +40,20 @@ export const nowysacz = {
             {
                 icon: "gis:location-poi",
                 iconText: "ZASIĘG LOKALNY",
-                title: `Zdobędziesz klientów w Nowym Sączu i okolicach`,
-                desc: `Zwiększ zasięg swojego biznesu i przyciągnij nowych klientów z Nowego Sącza.`
+                title: "Zdobędziesz klientów w Nowym Sączu i okolicach",
+                desc: "Zwiększ zasięg swojego biznesu i przyciągnij nowych klientów z Nowego Sącza."
             }
         ]
     },
 
     technology: {
-        title: "Bezpieczne systemy i&nbsp;technologie dla stron www w Nowym Sączu",
+        title: "Nowoczesne systemy i technologie dla stron www w Nowym Sączu",
         paragraph1: "Tworzymy funkcjonalne, estetyczne i w pełni responsywne witryny oparte na nowoczesnych technologiach, które gwarantują niezawodność, szybkość działania i bezproblemowe funkcjonowanie strony w każdych warunkach. Nasze projekty stron powstają z myślą o indywidualnych potrzebach klienta - Twoja witryna będzie nie tylko atrakcyjna wizualnie, ale przede wszystkim intuicyjna w obsłudze.",
-        paragraph2: "Wykorzystujemy rozwiązania przyspieszające ładowanie (minifikacja kodu, kompresja obrazów), dbamy o optymalizację SEO oraz w razie potrzeby implementujemy system CMS, dzięki któremu będziesz mógł/mogła samodzielnie edytować swoją stronę www. Dzięki temu wszystko działa płynnie na komputerach i urządzeniach mobilnych, a regularne aktualizacje i backupy chronią dane osobowe użytkowników."
+        paragraph2: "Wykorzystujemy rozwiązania przyspieszające ładowanie (minifikacja kodu, kompresja obrazów), dbamy o optymalizację SEO oraz w razie potrzeby implementujemy system CMS, dzięki któremu będziesz mógł/mogła samodzielnie edytować swoją stronę www. Zapewniamy przy tym kompleksową obsługę w zakresie aktualizacji, backupów i wsparcia technicznego."
     },
 
     processCity: {
-        title: "Proces projektowania i&nbsp;tworzenia stron www dla firm w Nowym Sączu",
+        title: "Proces projektowania i tworzenia stron internetowych dla firm w Nowym Sączu",
         paragraph1: "Proces tworzenia witryny dla firm w regionie to dla nas nie tylko kodowanie, lecz także budowanie silnej obecności online. Każdy etap realizacji jest dokładnie przemyślany, aby dostarczyć rozwiązanie spełniające oczekiwania Twoich klientów i użytkowników.",
         cta: "Chcesz zlecić budowę profesjonalnej strony www?",
         elements: [
@@ -64,13 +64,13 @@ export const nowysacz = {
             },
             {
                 icon: "solar:pallete-2-bold",
-                title: "Projekt graficzny",
-                desc: "Projektujemy nowoczesne, responsywne i przyciągające uwagę layouty, które wyróżnią Twoją markę. Każda witryna powstaje z myślą o przejrzystości i wygodzie użytkownika."
+                title: "Identyfikacja wizualna",
+                desc: "Projektujemy nowoczesne, responsywne i przyciągające uwagę layouty oparte na indywidualnym projekcie graficznym. Każda identyfikacja wizualna powstaje z myślą o przejrzystości i wygodzie użytkownika."
             },
             {
                 icon: "fa6-solid:laptop-code",
                 title: "Programowanie",
-                desc: "Wdrażamy stronę z użyciem najnowszych technologii. Dbamy o szybkość działania i bezpieczeństwo strony, zapewniając pełną funkcjonalność."
+                desc: "Wdrażamy stronę z użyciem najnowszych technologii. Dbamy o szybkość działania na każdym urządzeniu i bezpieczeństwo strony, zapewniając pełną funkcjonalność."
             },
             {
                 icon: "material-symbols:rocket",
@@ -81,8 +81,8 @@ export const nowysacz = {
     },
 
     offerCity: {
-        title: "Nasza oferta strony www dla firm z Nowego Sącza",
-        paragraph1: "Tworzymy nowoczesne, szybkie i przyjazne SEO strony internetowe, które przyciągną klientów z Nowego Sącza i okolic. Specjalizujemy się w:",
+        title: "Nasza oferta stron www i sklepów dla firm z Nowego Sącza",
+        paragraph1: "Tworzymy nowoczesne, szybkie i responsywne strony internetowe oraz sklepy, które przyciągną klientów z Nowego Sącza i okolic. Specjalizujemy się w:",
     },
 
     portfolio: {
@@ -97,12 +97,12 @@ export const nowysacz = {
         elements: [
             {
                 id: 1,
-                question: "Jak długo trwa stworzenie witryny?",
+                question: "Jak długo trwa stworzenie strony?",
                 answer: "Czas realizacji zależy od skomplikowania projektu oraz Twoich indywidualnych wymagań. Standardowo proces ten trwa od kilku tygodni do kilku miesięcy. Po wstępnej analizie potrzeb możemy przedstawić dokładniejszy harmonogram."
             },
             {
                 id: 2,
-                question: "Ile kosztuje strona w Nowym Sączu?",
+                question: "Ile kosztuje strona www i sklep internetowy?",
                 answer: "Koszt zależy od wielu czynników, takich jak zakres funkcjonalności, stopień skomplikowania projektu oraz dodatkowe usługi. Po zapoznaniu się z Twoimi wymaganiami przygotujemy indywidualną wycenę."
             },
             {
@@ -113,12 +113,12 @@ export const nowysacz = {
             {
                 id: 4,
                 question: "Jakie informacje są potrzebne do rozpoczęcia współpracy?",
-                answer: "Na początek potrzebujemy poznać Twoje oczekiwania i cele związane ze stroną, informacje o Twojej firmie, preferencje dotyczące designu oraz funkcjonalności, a także ewentualne materiały, takie jak logo czy treści."
+                answer: "Na początek potrzebujemy poznać Twoje oczekiwania i cele związane ze stroną, informacje o Twojej marce, preferencje dotyczące designu oraz funkcjonalności, a także ewentualne materiały, takie jak logo czy treści."
             },
             {
                 id: 5,
                 question: "Co mogę zyskać z dobrze wykonanej strony?",
-                answer: "Dobrze wykonana strona internetowa Twojej firmy pozwala wyróżnić się na tle konkurencji. Dzięki stronie internetowej zyskasz unikalny design, a odpowiednie SEO strony www zapewni lepszą widoczność w wyszukiwarce i przyciągnie więcej potencjalnych klientów."
+                answer: "Dobrze wykonana strona internetowa Twojego biznesu pozwala wyróżnić się na tle konkurencji. Dzięki stronie zyskasz unikalny design, a odpowiednie SEO strony www zapewni lepszą widoczność w wyszukiwarce i przyciągnie więcej potencjalnych klientów."
             },
         ]
     }
